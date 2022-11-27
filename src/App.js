430c37b01d993a82fc566769e613f04453b7b1a9
Michael D <dmika91@gmail.com>
2022-11-27 20:59:16 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import ActionButton from "./components/ActionButon";
 import ActionGroup from "./components/ActionGroup";
 import "./styles.css";
 
-const prefix = `https://192.168.1.38`;
+const prefix = `http://192.168.1.38`;
 
 export default function App() {
   return (
@@ -56,6 +56,56 @@ export default function App() {
           shortcuts={[
             { label: "Orange", values: [255, 65, 0] },
             { label: "White", values: [255, 255, 255] },
+          ]}
+        />
+        <ActionButton
+          label="Blink"
+          url={`${prefix}/animation/blink`}
+          params={[
+            { label: "R", type: "number", defaultValue: 255 },
+            { label: "G", type: "number", defaultValue: 255 },
+            { label: "B", type: "number", defaultValue: 255 },
+          ]}
+          shortcuts={[
+            { label: "Orange", values: [255, 65, 0] },
+            { label: "White", values: [255, 255, 255] },
+          ]}
+        />
+        <ActionButton label="None" url={`${prefix}/animation/none`} />
+      </ActionGroup>
+      <ActionGroup title="Execute">
+        <ActionButton
+          label="Color"
+          url={`${prefix}/animation/execute/color`}
+          params={[
+            { label: "R", type: "number", defaultValue: 255 },
+            { label: "G", type: "number", defaultValue: 65 },
+            { label: "B", type: "number", defaultValue: 0 },
+          ]}
+          shortcuts={[
+            { label: "Orange", values: [255, 65, 0] },
+            { label: "White", values: [255, 255, 255] },
+            { label: "Red", values: [191, 6, 0] },
+          ]}
+        />
+        <ActionButton
+          label="Speed"
+          url={`${prefix}/animation/execute/speed`}
+          params={[{ label: "Value", type: "number", defaultValue: 20 }]}
+          shortcuts={[
+            { label: "5", values: [5] },
+            { label: "10", values: [10] },
+            { label: "20", values: [20] },
+          ]}
+        />
+        <ActionButton
+          label="Trail"
+          url={`${prefix}/animation/execute/trail`}
+          params={[{ label: "Value", type: "number", defaultValue: 20 }]}
+          shortcuts={[
+            { label: "5", values: [5] },
+            { label: "10", values: [10] },
+            { label: "20", values: [20] },
           ]}
         />
       </ActionGroup>
